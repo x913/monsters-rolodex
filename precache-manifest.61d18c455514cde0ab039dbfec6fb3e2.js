8c90cb74437cb318dd50dec611ff11dbaf03e694
x913 <x913x913@gmail.com>
2019-09-10 11:16:58 +0500

    Updates

---
+++ precache-manifest.61d18c455514cde0ab039dbfec6fb3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ce0e5bad2f5e34430a265ab1659f47",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "78c0d0fe76dc6e41f688",
+    "url": "/monsters-rolodex/static/css/main.f8949a9e.chunk.css"
+  },
+  {
+    "revision": "0c88c0663ad77c4c524a",
+    "url": "/monsters-rolodex/static/js/2.6053dec2.chunk.js"
+  },
+  {
+    "revision": "78c0d0fe76dc6e41f688",
+    "url": "/monsters-rolodex/static/js/main.c514f7ea.chunk.js"
+  },
+  {
+    "revision": "9679b25fbb58365eb7b7",
+    "url": "/monsters-rolodex/static/js/runtime~main.c610535c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
